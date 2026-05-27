0eee12db8a4534a53d178e8d42c1f0b5296e42d4
Metalit <48568911+Metalit@users.noreply.github.com>
2026-05-26 20:54:03 -0600

    Run gersemi

--- cmake/windows/defaults.cmake
+++ cmake/windows/defaults.cmake
@@ -8,9 +8,12 @@ set(CMAKE_FIND_PACKAGE_TARGETS_GLOBAL TRUE)
 include(buildspec)
 
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-    string(REPLACE "\\" "/" ALLUSERSPROFILE_FIXED $ENV{ALLUSERSPROFILE})
-    set(CMAKE_INSTALL_PREFIX
-        "${ALLUSERSPROFILE_FIXED}/obs-studio/plugins"
-        CACHE STRING "Default plugin installation directory" FORCE
-    )
+  string(REPLACE "\\" "/" ALLUSERSPROFILE_FIXED $ENV{ALLUSERSPROFILE})
+  set(
+    CMAKE_INSTALL_PREFIX
+    "${ALLUSERSPROFILE_FIXED}/obs-studio/plugins"
+    CACHE STRING
+    "Default plugin installation directory"
+    FORCE
+  )
 endif()
